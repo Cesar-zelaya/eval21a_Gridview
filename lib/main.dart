212a809a48b821e0c381e0e0d7026313adec3d6d
cesarzelaya <139627259+Cesar-zelaya@users.noreply.github.com>
2023-10-22 18:01:00 -0600

    elimnando el contador y el boton

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,24 +47,15 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
 
         child: Column(
-         
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+     // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
