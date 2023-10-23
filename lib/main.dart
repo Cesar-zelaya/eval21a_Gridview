3dddd6d2049be0c57d0dff1fed8fb8c281970676
cesarzelaya <139627259+Cesar-zelaya@users.noreply.github.com>
2023-10-22 18:04:22 -0600

    eliminaccion de codigo que no se ocupara

--- lib/main.dart
+++ lib/main.dart
@@ -11,51 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-
-          ],
-        ),
-      ),
-     // This trailing comma makes auto-formatting nicer for build methods.
+      home: const MyHomePage(title: 'GridView'),
     );
   }
 }
